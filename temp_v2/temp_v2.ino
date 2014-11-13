d7d1e9746aa3d8c6dae0e1f9aece707f03e98853
Kim Caleb <kimcaleb421@gmail.com>
2014-11-13 17:04:55 -0600

    in progress 7-seg LED

--- temp_v2/temp_v2.ino
+++ temp_v2/temp_v2.ino
@@ -37,8 +37,8 @@ const byte ledCharSet[10] = {
 #define SLIDER1  A2 //Matches button 1
 #define SLIDER2  A1 
 #define SLIDER3  A0 //Matches button 3
-#define LIGHT    A3
-#define TEMP     A4
+#define LEDSCL   A5
+#define LEDSDA   A4
 
 #define FAN      2
 #define BUZZER   3
@@ -54,28 +54,35 @@ const byte ledCharSet[10] = {
 //v1.7 uses CapSense
 //This relies on the Capactive Sensor library here: http://playground.arduino.cc/Main/CapacitiveSensor
 #include <CapacitiveSensor.h>
+#include <Wire.h> 
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
 
 CapacitiveSensor capPadOn92 = CapacitiveSensor(9, 2);   //Use digital pins 2 and 9,
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-unsigned long timerled;
+unsigned long timerled; //controls led delay times
 int initcrit; //set initial condition for 
 boolean led_run1=false; //controls for leds
 boolean led_run2=true;
 boolean test = true;
 int count=0; //counter for heating safety mechanism
-
+Adafruit_7segment matrix = Adafruit_7segment();
 void setup()
 {
+  #ifndef __AVR_ATtiny85__
+  Serial.println("7 Segment Backpack Test");
+  #endif
+  matrix.begin(0x70);
   Serial.begin(9600);
 
   //Initialize inputs and outputs
   pinMode(SLIDER1, INPUT);
   pinMode(SLIDER2, INPUT);
   pinMode(SLIDER3, INPUT);
-  pinMode(LIGHT, INPUT);
-  pinMode(TEMP, INPUT);
-  pinMode(A5, INPUT);
+  //pinMode(LIGHT, INPUT);
+  //pinMode(TEMP, INPUT);
+  pinMode(A3, INPUT); //thermistor 
 
   //Enable internal pullups
   pinMode(BUTTON1, INPUT_PULLUP);
@@ -110,7 +117,7 @@ void setup()
 void loop()
 {
   //Read inputs
-  int sensorValue = analogRead(A5);
+  int sensorValue = analogRead(A3); //thermistor
   int val1 = analogRead(SLIDER1);
   int val2 = analogRead(SLIDER2);
   int val3 = analogRead(SLIDER3);
@@ -141,18 +148,20 @@ void loop()
   float tempvolt = sensorValue*(5.0/1023.0);
   //converting voltage to temperature value
   //float temperature = ((2*tempvolt/5)-.8)/-.21;
-  float temperature= 100*(3.044625*pow(10,-5)*pow(tempvolt,6)-(.005209376*pow(tempvolt,5))+0.065699269*pow(tempvolt,4)-0.340695972*pow(tempvolt,3)+0.897136183*pow(tempvolt,2)-1.419855102*tempvolt+1.451672296);
+  float temperature= 100*(3.044625*pow(10,-5)*pow(tempvolt,6)-(.005209376*pow(tempvolt,5))+0.065699269*pow(tempvolt,4)-0.340695972*pow(tempvolt,3)+0.897136183*pow(tempvolt,2)-1.419855102*tempvolt+1.451672296); //voltage to temp conversion eqn
+  matrix.print(temperature);
+  matrix.writeDisplay();
   Serial.println(temperature);
 
 //flashes LED alerts when temperature passes a set threshold value  
-  if (temperature < 27){
-    digitalWrite(HEAT, HIGH);
-    count++;
+  if (temperature < 27){ //if temp is too low
+    digitalWrite(HEAT, HIGH); //heater turns on
+    count++; //safety counter starts
     Serial.print(count);
     Serial.print(", ");
-    if (count >25){
-      digitalWrite(HEAT, LOW);
-      if (count >50)
+    if (count >25){ //if the heater is on for x amount of time
+      digitalWrite(HEAT, LOW); //heater automatically turns off after the x amount of time
+      if (count >50) //resets the counter after a certain amount of time
         count = 0;}}
   else
     digitalWrite(HEAT, LOW);
@@ -161,7 +170,7 @@ void loop()
   {
     tone(BUZZER, 262); //alarm turns on
     Serial.print(" It's too hot!");
-    digitalWrite(FAN, LOW); //turns on fan
+    digitalWrite(FAN, HIGH); //turns on fan
     if (initcrit==0){ //initial case to set up led differences and overall timer (timerled)
       timerled=millis(); //built-in overall timer for led delays
       initcrit = 1;
@@ -176,7 +185,7 @@ void loop()
             
   }
   else{
-    digitalWrite(FAN, HIGH); //turns off the fan
+    digitalWrite(FAN, LOW); //turns off the fan
     digitalWrite(LED1, LOW); //turns off the leds
     digitalWrite(LED2, LOW);
     noTone(BUZZER); //turns off the buzzer
@@ -234,5 +243,9 @@ void loop()
   }*/
 }
 
+
+//Serial.print("blah blah:" + variable);
+//Serial.println(variable);
+//Serial.
 
 
